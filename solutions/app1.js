81f411c7aa1aff34e5b06f3343b4abc3d8f366af
utnesz <90829509+utnesz@users.noreply.github.com>
2022-08-18 20:29:43 +0200

    ready

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,5 +1,3 @@
+const getSecondsOfThisYearUntilNow = () => Math.round((new Date('August 31, 2022').getTime() - new Date(`January 01, 2022`).getTime()) / 1000);
 
-
-
-
-// export default getSecondsOfThisYearUntilNow;
+export default getSecondsOfThisYearUntilNow
